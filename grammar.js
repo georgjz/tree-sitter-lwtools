0b9498003d5849f0aa4995d28b65336debedbc8a
Georg Ziegler <georgjziegler@gmail.com>
2018-10-10 01:16:45 +0200

    Basic file structure

---
+++ grammar.js
@@ -0,0 +1,17 @@
+module.exports = grammar ({
+    name: 'lwtools6809',
+
+    rules: {
+        // the production rules for lwtools
+        source_file: $ => 'hello'
+    }
+});
+
+//
+// module.exports = grammar ({
+//     name: 'lwtools-6809',
+//
+//     rules: {
+//         // the production rules for lwtools
+//     }
+// });
