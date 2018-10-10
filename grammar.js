b4d90e9323737c46949b472cc17f2a1e7450cce6
Georg Ziegler <georgjziegler@gmail.com>
2018-10-10 03:11:18 +0200

    Add basic line format, recognize labels and opcode fields

--- grammar.js
+++ grammar.js
@@ -2,16 +2,44 @@ module.exports = grammar ({
     name: 'lwtools6809',
 
     rules: {
-        // the production rules for lwtools
-        source_file: $ => 'hello'
+        // a source file is parsed line by line
+        source_file: $ => repeat1($._line),
+
+        // a line has up to four fields
+        _line: $ => seq(
+            optional(seq($.label, ':')),    // label field
+            optional($._instruction),       // opcode, assembler directive, or pseudo opcode
+            optional($._operand),           // operand(s)
+            optional($._comment),           // commend field
+            $._line_break                   // end of line
+        ),
+
+        // line break
+        _line_break: $ => '\n',
+
+        // labels, identifiers, symbols
+        // TODO: Check label length, ".:" legal?
+        label: $ => seq(optional(/[\@\?]/), $._identifier),
+        _identifier: $ => /[a-zA-Z\.\_][a-zA-Z0-9\.\_\$]*/,
+
+        // instructions
+        _instruction: $ => choice(
+            $.opcode
+        ),
+
+        // opcode
+        opcode: $ => 'nop',
+
+        // operand
+        _operand: $ => 'operand',
+
+        // comments
+        _comment: $ => choice(
+            $.line_comment
+        ),
+
+        // line comments
+        line_comment: $ => /\*.*/
+
     }
 });
-
-//
-// module.exports = grammar ({
-//     name: 'lwtools-6809',
-//
-//     rules: {
-//         // the production rules for lwtools
-//     }
-// });
